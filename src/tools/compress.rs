d13d9caf1cbedd06c582b5e5b20ae6f31b481ecd
Juli_32000 <77584146+juli3200@users.noreply.github.com>
2023-12-23 16:05:23 +0100

    changed name to evolan

--- src/tools/compress.rs
+++ src/tools/compress.rs
@@ -1,25 +1,9 @@
 
-use rar:: Archive;
+use lz4_flex;
 use std::path::Path;
 
+
+// archive folder in a tar file and compress it with lz4
 pub fn compress(path: String){
 
-    let folder_to_compress = &format!("{}", path);
-    let output_rar = "./output_archive.rar";
-
-    let archive = Archive::into(self);
-
-    if let Ok(mut writer) = archive.archive(output_rar) {
-        if let Err(err) = writer.append_path_with_name(Path::new(folder_to_compress), "") {
-            eprintln!("Failed to compress folder: {}", err);
-        } else {
-            if let Err(err) = writer.finalize() {
-                eprintln!("Failed to save archive: {}", err);
-            } else {
-                println!("Folder compressed to {}", output_rar);
-            }
-        }
-    } else {
-        eprintln!("Failed to create archive");
-    }
 }
